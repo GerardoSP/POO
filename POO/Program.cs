e96ad829b2aa63b805712e538a957b96c6f032d1
Gerardo Seoane <gerardo.seoane@finning.com>
2022-05-12 10:57:51 -0500

    Clase Numero 1 POO

---
+++ POO/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculoDeAreas;
+using Personas;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Ponce = new Persona();
+
+            Ponce.Nombre = "Cristian Antonio";
+            Ponce.Edad = 28;
+            Ponce.Altura = 1.84m;
+
+            Persona Marcos = new Persona();
+
+            Marcos.Nombre = "Marcos";
+            Marcos.Edad = 27;
+            Marcos.Altura = 1.60m;
+
+            Persona Gerardo = new Persona();
+
+            Console.WriteLine($"Accion--> {Ponce.Correr()}");
+
+            Ponce.Correr();
+            AreaTriangulo(10.5m, 10.00m);
+
+            Console.ReadKey();
+        }
+
+        
+        
+        static decimal AreaTriangulo(decimal bases , decimal altura)
+        {
+            return (bases*altura)/2;
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # POO
 Repositorio clases POO
+
+Aqui en este repositorio encontraran los ejercicios que haremos durante la clase, solo tienen que clonarlo en su local y crearse una nueva rama.
